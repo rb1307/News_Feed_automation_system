f05b4fee042ae667fc5a93867b5489ce32c51427
romit <romit.bhattacharyya@dataweave.com>
2020-12-07 12:38:03 +0530

    clustering commit -2

--- Modules/News_clustering/clustering.py
+++ Modules/News_clustering/clustering.py
@@ -1,20 +1,43 @@
+import sys
+sys.path.append("..")
 import connect_db
-from pymongo import MongoClient
-import json
-# import spacy
-# from spacy import displacy
-# import en_core_web_sm
-# nlp = en_core_web_sm.load()
-
-
-import itertools
-import operator
+from News_clustering import c_functions
 
 
 class NewsClusters:
     def __init__(self):
-        self.extract_db = connect_db.extracted_dbinstance()
+        extract_db = connect_db.extracted_dbinstance()
+        self.last_batch_articles = extract_db.find({})
+        # self.number_of_articles = self.last_batch_articles.count()
 
+    def similarity_matrix(self):
+        similarity_matrix = c_functions.generate_similarity_matrix(docs=self.last_batch_articles)
+        threshold = c_functions.generate_threshold(similarity_value_matrix=similarity_matrix)
+        return similarity_matrix, threshold
 
     def generate_clusters(self):
-        similarity_matrix =
+        clusters = []
+        similarity_matrix, threshold = self.similarity_matrix()
+        # print(len(self.number_of_articles))
+        number_of_articles = len(similarity_matrix)
+        input_article = 0
+        clustered_article_no = []
+        while input_article < number_of_articles:
+            clustered_article_no = list(set(clustered_article_no))
+            if input_article not in clustered_article_no:
+                current_cluster = [input_article]
+                similar_articles = c_functions.similar_docs(doc_no=input_article, similarity_matrix=similarity_matrix,
+                                                            threshold=threshold, cluster=current_cluster)
+
+                clustered_article_no.extend(similar_articles)
+                clusters.append(current_cluster)
+            input_article = input_article + 1
+        return clusters
+
+
+def getresponse():
+    obj = NewsClusters()
+    obj.generate_clusters()
+
+
+getresponse()
