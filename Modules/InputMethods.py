cde0a9974ba49646c1230719c1cea3913dc092e3
Romit <romitb1307@gmail.com>
2021-06-25 17:33:26 +0530

    Function to input excel data

--- Modules/InputMethods.py
+++ Modules/InputMethods.py
@@ -1,9 +1,21 @@
 import json
-from CustomErrors import *
+import pandas as pd
+from Modules.CustomErrors import *
+
+
+def input_excel(file_name=None, path=None):
+    # input_data = pd.DataFrame()
+    absolute_path = path + "/" + file_name
+    try:
+        input_data = pd.read_excel(absolute_path)
+        return input_data
+    except Exception as e:
+        print(e)
+        return input_data
 
 
 def input_json(path=None, file_name=None):
-    file_name= path + file_name
+    file_name = path + file_name
     try:
         with open(file_name, mode='r') as f:
             data = json.load(f)
@@ -12,11 +24,11 @@ def input_json(path=None, file_name=None):
         raise InputDataError()
 
 
-def getdb_credentials(path=None, file_name=None):
-    file_name = path + file_name
+def get_db_credentials(path=None, file_name=None):
+    file_name = path + '/'+file_name
     try:
         with open(file_name, mode='r') as f:
-            data=json.load(f)
+            data = json.load(f)
         return data.get("username"), data.get("password")
     except Exception:
-        raise InputDataError
+        raise InputDataError()
