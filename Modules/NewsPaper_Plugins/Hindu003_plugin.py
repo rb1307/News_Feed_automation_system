11f13ad0ce3a6ca0b287d0bb71335c4ed30732a5
Romit <romitb1307@gmail.com>
2021-06-29 10:18:04 +0530

    Directory Restructure

--- Modules/NewsPaper_Plugins/Hindu003_plugin.py
+++ Modules/NewsPaper_Plugins/Hindu003_plugin.py
@@ -1,17 +1,17 @@
-from GenericCrawlerandExtractor import GCA
-from InputMethods import input_json
-from CommonFunctions import clean_article_body
+from Modules import GenericCrawlerandExtractor
+from Modules import InputMethods
+from Modules import CommonFunctions
 
-CONFIG_PATH = '/home/hp/NFA-System/Modules/NewsPaper_configs/'
+CONFIG_PATH = '/home/hp/NFA-System/Modules/NewsPaper_configs'
 CONFIG_FILE = 'configs_The Hindu.json'
 
 
-class Hindu(GCA):
+class Hindu(GenericCrawlerandExtractor.GCA):
     def __init__(self, **kwargs):
-        self.source_configs ={}
+        self.source_configs = {}
         self.source_configs.update(kwargs)
-        config_data = input_json(path=CONFIG_PATH, file_name=CONFIG_FILE)
-        self.source_configs['extractor_configs'] =config_data
+        config_data = InputMethods.input_json(path=CONFIG_PATH, file_name=CONFIG_FILE)
+        self.source_configs['extractor_configs'] = config_data
         super().__init__(**self.source_configs)
         self.xml_tree = super().convertresponsetoxmltree()
 
@@ -23,13 +23,13 @@ class Hindu(GCA):
         a_id = self.getarticleidfromurl()
         xpath = "//div[@id='content-body-14269002-" + str(a_id) + "']//text()"
         body = self.xml_tree.xpath(xpath)
-        article_body = clean_article_body(body_list=body)
+        article_body = CommonFunctions.clean_article_body(body_list=body)
         return article_body
 
 
 def getsourceresponse(**kwargs):
     obj = Hindu(**kwargs)
-    resp= {'article_id': obj.getarticleidfromurl(), 'image': obj.extractimagelink_xml(),
+    resp = {'article_id': obj.getarticleidfromurl(), 'image': obj.extractimagelink_xml(),
            'provided_keywords': obj.extractkeywords_regex(), 'article_body': obj.extractarticlebody_xml(),
            'source_id': kwargs.get("source_id")}
     return resp
